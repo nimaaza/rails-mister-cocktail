e144b2b0d26b9aea0a0a16d618637c66036002c9
Nima Azarbayjani <nimaza@gmail.com>
2020-11-04 23:33:30 +0330

    made corrections to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :doses, only: :destroy
 end
