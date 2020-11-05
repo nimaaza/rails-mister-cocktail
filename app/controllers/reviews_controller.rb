7a45aee44677ebc6be8454783525c8713d525fc0
Nima Azarbayjani <nimaza@gmail.com>
2020-11-05 22:46:29 +0330

    added feature to add reviews from cocktail show pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review = Review.new(review_params)
+    @review.cocktail = @cocktail
+
+    if @review.save
+      redirect_to @cocktail
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :cocktails, only: %i[index show new create] do
     # resources :doses, only: %i[new create]
-    resources :doses, only: %i[create]
+    resources :doses, only: :create
+    resources :reviews, only: :create
   end
 
   resources :doses, only: :destroy
