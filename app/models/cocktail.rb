afd51e00838288e61cf18a985d93082b0bc9d85e
Nima Azarbayjani <nimaza@gmail.com>
2021-01-27 12:27:16 +0330

    setup cloudinary image service & ActiveStorage

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,6 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :ingredients, through: :doses
+
+  has_one_attached :photo
 end
