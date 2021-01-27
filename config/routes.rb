eca44ded749d1fd7b193b48196f4422ba53ae16c
Nima Azarbayjani <nimaza@gmail.com>
2021-01-27 11:20:16 +0330

    add a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   resources :cocktails, only: %i[index show new create] do
-    # resources :doses, only: %i[new create]
     resources :doses, only: :create
     resources :reviews, only: :create
   end
